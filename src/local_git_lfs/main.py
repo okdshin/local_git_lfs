6fb9eb16007c7397008b980c73febebb35a137b8
okada <kokuzen@gmail.com>
2024-10-12 23:18:40 +0900

    argparse

--- src/local_git_lfs/main.py
+++ src/local_git_lfs/main.py
@@ -1,23 +1,34 @@
 import aiofiles
+import argparse
+from datetime import datetime
 from enum import Enum
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.wsgi import WSGIMiddleware
 from flask import Flask
 import hashlib
+import os
 from pathlib import Path
 from pydantic import BaseModel
 import shutil
 from typing import Dict, List, Union
+import uuid
+import uvicorn
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--host", type=str, default="0.0.0.0")
+parser.add_argument("--port", type=int, default=8000)
+parser.add_argument("--git-object-dir", type=str, default="tmp")
+parser.add_argument("--max-git-object-size", type=int, default=20*(1024**3))  # default is 20GB
+args = parser.parse_args()
 
 
 app = FastAPI()
 
 
-git_object_dir = Path("./tmp")
-
-
-MAX_GIT_OBJECT_SIZE = 20 * (1024**3)  # 20GB
+GIT_OBJECT_DIR = Path(args.git_object_dir)
+MAX_GIT_OBJECT_SIZE = args.max_git_object_size
 
 
 class OperationEnum(str, Enum):
@@ -77,7 +88,7 @@ async def batch(request: BatchRequest, raw_request: Request):
     response = BatchResponse(objects=[])
     for git_object in request.objects:
         if git_object.size > MAX_GIT_OBJECT_SIZE:
-            raise HTTPException()
+            raise HTTPException(status_code=422)  # Unprocessable entity
 
         if request.operation == OperationEnum.upload:
             action = UploadAction(href=f"{base_url}/objects/{git_object.oid}")
@@ -112,13 +123,20 @@ async def upload_object(oid: str, request: Request):
         print("already exist. uploading is not executed")
     else:
         m = hashlib.sha256()
-        async with aiofiles.open(git_object_dir / oid, "wb") as out_file:
+        unique_string = f"{uuid.uuid4().hex}_{int(datetime.now().timestamp())}"
+        temp_object_file_path = GIT_OBJECT_DIR / f"{oid}.{unique_string}"
+        file_size = 0
+        async with aiofiles.open(temp_object_file_path, "wb") as out_file:
             async for chunk in request.stream():
+                file_size += len(chunk)
+                if file_size > MAX_GIT_OBJECT_SIZE:
+                    raise HTTPException(status_code=400, detail="file size is too large")
                 m.update(chunk)
                 await out_file.write(chunk)
         if m.hexdigest() != oid:
-            remove_git_object(oid=oid)
+            temp_object_file_path.unlink()
             raise HTTPException(status_code=400, detail="hash does not match")
+        os.replace(temp_object_file_path, GIT_OBJECT_DIR / oid)  # os.replace() is atomic operation
 
 
 @app.get("/objects/{oid}")
@@ -128,17 +146,17 @@ async def download_object(oid: str):
         raise HTTPException(status_code=404, detail="git object is not found")
 
     def iterfile():
-        with open(git_object_dir / oid, mode="rb") as f:
+        with open(GIT_OBJECT_DIR / oid, mode="rb") as f:
             yield from f
     return StreamingResponse(iterfile(), media_type="application/octet-stream")
 
 
 def git_object_exists(oid: str) -> bool:
-    return (git_object_dir / oid).exists()
+    return (GIT_OBJECT_DIR / oid).exists()
 
 
 def remove_git_object(oid: str) -> None:
-    shutil.rmtree(git_object_dir / oid)
+    shutil.rmtree(GIT_OBJECT_DIR / oid)
 
 
 # flask frontend
